676c7893b0ca350acf4a031259169f71463a2528
Shubham Malhan <shubhammalhan00@gmail.com>
2024-09-01 23:35:06 +0530

    learn about import in react

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,10 +1,13 @@
-
+import Chai from './Chai'
 
 function App() {
   return (
-    <h1>CHAI AUR CODE
-      <h1>hello</h1>
-   </h1>
+    <>
+      <Chai />
+      <h1>CHAI AUR CODE
+        <h1>hello</h1>
+      </h1>
+    </>
   );
 }
 
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,7 @@
+export default function Chai() {
+    return (
+        <h1>Coffee Aur React</h1>
+    )
+}
+
+// export default Chai;
