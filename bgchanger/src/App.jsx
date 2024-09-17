285782ebd1d4a4e93f20fd8993974e076de40d3d
Shubham Malhan <shubhammalhan00@gmail.com>
2024-09-17 17:22:38 +0530

    working on react

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setcolor] = useState('black')
+
+  return (
+    <>
+     
+      
+    </>
+  )
+}
+
+export default App
