1bde12c02e2e872229d2b0250bc8fdaa63efcd9e
Shubham Malhan <shubhammalhan00@gmail.com>
2025-02-20 23:07:44 +0530

    learn about react route

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './components/Home/home.jsx'
+import {BrowserRouter, Routes, Route} from 'react-router'
+import About from './components/About/about.jsx'
+import Contact from './components/Contact/contact.jsx'
+import User from './components/User/user.jsx'
+import Github from './components/GitHub/github.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<App />} >
+          <Route index element = {<Home />} />
+          <Route path='about' element= {<About />} />
+          <Route path='contact' element = {<Contact />} />
+          <Route path='param/:userid' element = {<User />} />
+          <Route path='github' element = {<Github />} />
+      </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
