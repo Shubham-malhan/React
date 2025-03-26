5d1c504a25744b31670497c83af9622f55d8f5f7
Shubham Malhan <shubhammalhan00@gmail.com>
2025-03-26 23:24:32 +0530

    learning redux toolkit

---
+++ reduxToolkitTodo/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', // or wherever your HTML files are located
+    './src/**/*.{html,js,jsx,ts,tsx}', // adjust according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
